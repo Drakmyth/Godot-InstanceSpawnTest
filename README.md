8ea5c5055754a8db432f00eccaa7bd9650fb5648
Drakmyth <shaun.hamman@gmail.com>
2020-01-25 23:13:31 -0800

    Added controls to readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Godot-InstanceSpawnTest
+
+* Left-click actor to destroy
+* 'R' to respawn all actors
--- Spawnable.gd
+++ Spawnable.gd
@@ -4,7 +4,6 @@ func _ready():
 	# warning-ignore:return_value_discarded
 	 connect("area_entered", self, "_on_area_entered")
 
-# warning-ignore:unused_argument
 func _on_area_entered(area):
 	if area.name == "MouseArea":
 		queue_free()
